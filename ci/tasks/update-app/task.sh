caf492a92708225f4e34f7ca04af1e375dbbcab0
Michael Lieser <michael.lieser@gmx.de>
2019-09-10 17:24:59 +0200

    Update app

--- ci/tasks/update-app/task.sh
+++ ci/tasks/update-app/task.sh
@@ -4,4 +4,21 @@ set -o errexit # Exit immediately if a simple command exits with a non-zero stat
 set -o nounset # Report the usage of uninitialized variables
 
 echo "Update App"
-exit 1
+
+updated_app_repository_dir=${PWD}/updated-app
+app_tag=$(<${PWD}/app-release/tag)
+
+echo 'Clone repository...'
+# We can't clone the repository in all cases because of the
+# submodules. For this we would need a git private key and
+# so on. Therefore, we copy it and so we are safe.
+rsync -a ${PWD}/boshrelease/ ${updated_app_repository_dir}
+cd ${updated_app_repository_dir}
+
+echo 'Update app submodule...'
+
+echo 'Commit updated app...'
+git add src/cfeu19-sample-app
+git commit -m "Update App to ${app_tag}"
+
+echo 'Done'
